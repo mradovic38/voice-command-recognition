cba4cb31fbf92c33cb74df53dbfdf04d7d981c21
mradovic38 <mihailo.radovic11@gmail.com>
2024-12-24 00:41:35 +0100

    Integrate out-of-vocabulary handling using dtw to main application in gui.py and run.py. Improve feature_extraction.py, and  oov_check.py

--- feature_extraction.py
+++ feature_extraction.py
@@ -1,23 +1,29 @@
-
 import librosa
 import numpy as np
 
-def extract_features(y: np.ndarray, sr:int=16000) -> np.ndarray:
+def extract_features(y: np.ndarray, sr: int = 16000) -> np.ndarray:
     """
-    Extract audio features from a file using MFCC.
+    Extract audio features from a file using MFCC
     
     Args:
-        file_path (str): Path to the audio file
+        y (np.ndarray): Audio signal
+        sr (int): Sample rate (default: 16000)
     
     Returns:
-        numpy.ndarray: Extracted features
+        np.ndarray: Extracted features (MFCC)
     """
+    # Ensure there are no zero values to avoid log of zero errors
     y[y == 0] = 1e-10
-    y = librosa.util.normalize(y)
-
-    # MFCC extraction with normalization
-    mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13, n_fft=2048, hop_length=512)
-    mfccs = (mfccs - np.mean(mfccs)) / np.std(mfccs)
-    features = mfccs.T
     
-    return features
+    # Normalize audio signal
+    y = librosa.util.normalize(y)
+    
+    
+    # MFCC extraction
+    mfccs = librosa.feature.mfcc(y=y, sr=sr, n_mfcc=13)
+
+    # Normalize MFCCs individually per feature
+    mfccs = (mfccs - np.mean(mfccs, axis=1, keepdims=True)) / np.std(mfccs, axis=1, keepdims=True)
+    
+    
+    return mfccs.T  # Transpose to match the expected (time x feature) format
